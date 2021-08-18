b945bf6028616046da1232fda3045ab69ece5091
Kaiying Fu <kaiying.fu@gmail.com>
2021-08-18 16:14:42 +0800

    Implement Dependency Injection

--- 5c-Dependency-Injection/src/app.js
+++ 5c-Dependency-Injection/src/app.js
@@ -1,24 +1,22 @@
 const express = require('express');
 const logger = require('morgan');
-require('dotenv').config();
 
-const app = express();
-app.use(express.json());
-app.use(logger('common'));
+module.exports = (router) => {
+  const app = express();
+  app.use(express.json());
+  app.use(logger('common'));
 
-app.use(require('./routes'));
+  app.use(router);
 
-app.use((error, req, res, next) => {
-  if (error.type === 'malformed-request') {
-    console.log(error.msg);
-    res.status(400).send(error);
-  } else {
-    console.log('Some error occured');
-    res.status(500).send(error);
-  }
-});
+  app.use((error, req, res, next) => {
+    if (error.type === 'malformed-request') {
+      console.log(error.msg);
+      res.status(400).send(error);
+    } else {
+      console.log('Some error occured');
+      res.status(500).send(error);
+    }
+  });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+  return app;
+};
--- 5c-Dependency-Injection/src/routes/index.js
+++ 5c-Dependency-Injection/src/routes/index.js
@@ -1,73 +1,74 @@
 const router = require('express').Router();
-const db = require('../db');
 
-router.get('/items', async (req, res, next) => {
-  const items = await db.findAllItems();
-  res.status(200).send(items);
-});
+module.exports = (db) => {
+  router.get('/items', async (req, res, next) => {
+    const items = await db.findAllItems();
+    res.status(200).send(items);
+  });
 
-router.post('/items', async (req, res, next) => {
-  const { quantity, name } = req.body;
-  try {
-    const item = await db.insertItem({ quantity, name });
-    res.status(201).send(item);
-  } catch (error) {
-    error.msg = `Failed to create record for ${name || 'item with no name'}`;
-    error.type = 'malformed-request';
-    next(error);
-  }
-});
-
-router.get('/items/:id', async (req, res, next) => {
-  const id = req.params.id;
-  try {
-    const item = await db.findItem(id);
-    if (item) {
-      res.status(200).send(item);
-    } else {
-      throw new Error();
+  router.post('/items', async (req, res, next) => {
+    const { quantity, name } = req.body;
+    try {
+      const item = await db.insertItem({ quantity, name });
+      res.status(201).send(item);
+    } catch (error) {
+      error.msg = `Failed to create record for ${name || 'item with no name'}`;
+      error.type = 'malformed-request';
+      next(error);
     }
-  } catch (error) {
-    error.msg = `Item (id: ${id}) not found`;
-    error.type = 'malformed-request';
-    next(error);
-  }
-});
+  });
 
-router.put('/items/:id', async (req, res, next) => {
-  const { quantity, name } = req.body;
-  const id = req.params.id;
-  try {
-    const item = await db.updateItem(id, { quantity, name });
-    res.status(201).send(item);
-  } catch (error) {
-    error.msg = `Failed to update record for ${name || 'item with no name'}`;
-    error.type = 'malformed-request';
-    next(error);
-  }
-});
-
-router.delete('/items/:id', async (req, res, next) => {
-  const id = req.params.id;
-  try {
-    const success = await db.deleteItem(id);
-    if (success) {
-      res.status(201).send(`Item (id: ${id} deleted successfully)`);
-    } else {
-      throw new Error();
+  router.get('/items/:id', async (req, res, next) => {
+    const id = req.params.id;
+    try {
+      const item = await db.findItem(id);
+      if (item) {
+        res.status(200).send(item);
+      } else {
+        throw new Error();
+      }
+    } catch (error) {
+      error.msg = `Item (id: ${id}) not found`;
+      error.type = 'malformed-request';
+      next(error);
     }
-  } catch (error) {
-    error.msg = `Item (id: ${id}) not found`;
-    error.type = 'malformed-request';
-    next(error);
-  }
-});
+  });
 
-router.all('/items/*', (req, res, next) => {
-  res
-    .status(404)
-    .set({ 'Content-Type': 'text/html' })
-    .send('<h1>No Page Found</h1><a href="/items">Back</a>');
-});
+  router.put('/items/:id', async (req, res, next) => {
+    const { quantity, name } = req.body;
+    const id = req.params.id;
+    try {
+      const item = await db.updateItem(id, { quantity, name });
+      res.status(201).send(item);
+    } catch (error) {
+      error.msg = `Failed to update record for ${name || 'item with no name'}`;
+      error.type = 'malformed-request';
+      next(error);
+    }
+  });
 
-module.exports = router;
+  router.delete('/items/:id', async (req, res, next) => {
+    const id = req.params.id;
+    try {
+      const success = await db.deleteItem(id);
+      if (success) {
+        res.status(201).send(`Item (id: ${id} deleted successfully)`);
+      } else {
+        throw new Error();
+      }
+    } catch (error) {
+      error.msg = `Item (id: ${id}) not found`;
+      error.type = 'malformed-request';
+      next(error);
+    }
+  });
+
+  router.all('/items/*', (req, res, next) => {
+    res
+      .status(404)
+      .set({ 'Content-Type': 'text/html' })
+      .send('<h1>No Page Found</h1><a href="/items">Back</a>');
+  });
+
+  return router;
+};
